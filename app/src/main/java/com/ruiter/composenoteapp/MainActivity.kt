cbba979bf780dbd6b0746ffc087bab1ee894d580
Ruiter <ruitercomp@gmail.com>
2022-04-13 09:56:44 -0300

    add a state to input text and add a button

--- app/src/main/java/com/ruiter/composenoteapp/MainActivity.kt
+++ app/src/main/java/com/ruiter/composenoteapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ruiter.composenoteapp.screen.NoteScreen
 import com.ruiter.composenoteapp.ui.theme.ComposeNoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    NoteScreen()
                 }
             }
         }
--- app/src/main/java/com/ruiter/composenoteapp/screen/NoteScreen.kt
+++ app/src/main/java/com/ruiter/composenoteapp/screen/NoteScreen.kt
@@ -9,7 +9,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.BlendMode.Companion.Color
@@ -18,10 +18,20 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ruiter.composenoteapp.R
+import com.ruiter.composenoteapp.components.NoteButton
 import com.ruiter.composenoteapp.components.NoteInputText
 
 @Composable
 fun NoteScreen() {
+
+    var title by remember {
+        mutableStateOf("")
+    }
+
+    var description by remember {
+        mutableStateOf("")
+    }
+
     Column(modifier = Modifier.padding(8.dp)) {
         TopAppBar(title = {
             Text(text = stringResource(id = R.string.app_name))
@@ -32,8 +42,25 @@ fun NoteScreen() {
             )
         }, backgroundColor = Color(0xFFDADFE3))
 
-        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            NoteInputText(text = "hello", label = "Hello", onTextChange = {})
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            NoteInputText(
+                modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
+                text = title,
+                label = "Title",
+                onTextChange = {
+                    if (it.all { char -> char.isLetter() || char.isWhitespace() }) title = it
+                })
+            NoteInputText(
+                modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
+                text = description,
+                label = "Add a description",
+                onTextChange = {
+                    if (it.all { char -> char.isLetter() || char.isWhitespace() }) description = it
+                })
+            NoteButton(text = "Save", onClick = { /*TODO*/ })
         }
     }
 }
